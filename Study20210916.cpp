ec81d716078ea29fd810c96e8f9fcbc284251fb5
johnleest <johnleest94@gmail.com>
2021-09-16 15:05:19 +0900

    plus study

--- Study20210916.cpp
+++ Study20210916.cpp
@@ -1,8 +1,11 @@
-﻿
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    cout << 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 << endl;
+
 }
